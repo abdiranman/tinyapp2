7310158a217c108470c0e8e4dc8e5dfd796cc03d
abdiranman <abdirahman.abdi330@.com>
2023-08-27 07:23:13 -0400

    Registration Errors handled

--- express_server.js
+++ express_server.js
@@ -11,8 +11,27 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 //This function is used to generate a random string for the shortURL
 const generateRandomString = () => {
-    return Math.random().toString(36).substr(2, 6);
+    
+    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
 };
+
+const users = {
+    userRandomID: {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur"
+    },
+    user2RandomID: {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk"
+    }
+  };
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com",
@@ -30,26 +49,33 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
+    // This variable is used to get the user from the users object
+  const user = users[req.cookies["user_id"]]
     
 const templateVars = {
         urls: urlDatabase,
-        username: req.cookies["username"]
+        user
       };
     res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
     
-    const templateVars = { username: req.cookies["username"]};
+    // This variable is used to get the user from the users object
+  const user = users[req.cookies["user_id"]]
+  // This variable is used to pass the user object to the urls_new.ejs file
+  const templateVars = { user };
   res.render("urls_new", templateVars);
 });
 app.get("/urls/:id", (req, res) => {
+    const user = users[req.cookies["user_id"]];
     
     const templateVars = { 
         id: req.params.id, 
         longURL: urlDatabase[req.params.id],
-        username: req.cookies["username"]};
+        user };
     res.render("urls_show", templateVars);
 });
+
 app.post("/urls", (req, res) => {
     //Used to create a random string for the shortURL
     const shortURL = generateRandomString();
@@ -81,22 +107,71 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-    //This is used to get the username from the form
     const username = req.body.username;
-    //This is used to set the username as a cookie
-    res.cookie("username", username);
+   
+    const user = getUserByUsername(username);
 
+  // This is used to check if the user exists
+  if (user) {
+    res.cookie("user_id", user.id);
     res.redirect("/urls");
+  } else {
+    res.status(400).send("User not found");
+  }
 });
+// Helper function to get user object by username
+function getUserByUsername(username) {
+    for (const userId in users) {
+      const user = users[userId];
+      if (user.username === username) {
+        return user;
+      }
+    }
+    return null;
+  }
 
 // This is used to clear the cookie when the user clicks on the logout button
 app.post("/logout", (req, res) => {
-    res.clearCookie("username");
-    res.redirect("/urls");
+    res.cookie("user_id", "");
+res.redirect("/urls");
   });
 
   // This route is used to render the register page
 app.get("/register", (req, res) => {
 
     res.render("register");
+  })
+
+// This route handles the registration form data
+app.post('/register', (req,res) => {
+    const id = generteRandomString();
+    // This is used to get the email and password from the form
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    // this is used to check if the email or password is empty 
+    if (!email || !password) {
+      res.status(400).send("Email or password is empty");
+      return;
+    }
+  
+    // This is used to check if the email is already in the database
+    if (getUserByEmail(email)) {
+      res.status(400).send("Email is already in the database");
+      return;
+    }
+  
+    const newUser = {
+      id,
+      email,
+      password
+    }
+    // This is used to check if the email is already in the database
+    users[id] = newUser;
+    // This is used to set the user_id cookie
+    res.cookie("user_id", id);
+  
+    // This is used to redirect the user to the urls page
+    res.redirect("/urls");
+  
   })
